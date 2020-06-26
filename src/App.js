8dcfaa67b75ae29023bdb8daa52f5b888e584186
UdaHarry <harryrahmansyah97@gmail.com>
2020-06-26 17:11:11 +0700

    part 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Web3 from 'web3';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  componentWillMount(){
+    this.loadBlochainData();
+  }
+
+  async loadBlochainData(){
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const network = await web3.eth.net.getNetworkType();
+    
+    // fetch accoutn
+    const account = await web3.eth.getAccounts();
+    this.setState({account: account.toString()});
+  }
+  
+  constructor(props){
+    super(props)
+    this.state = {account: ''}
+  }
+  
+  render(){
+    return(
+      <div className="container">
+        <h1>Hello, World</h1>
+        <p>Your Accounts : {this.state.account}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
